a75aa39b4ec211cc98a73cb685b4b136a6960319
giri929 <reddygirish929@gmail.com>
2018-02-11 23:42:43 +0530

    Intialization of scenes

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour {
+
+	public void Loadlevel(string name){
+		Application.LoadLevel (name);
+	}
+
+	public void Exitrequest(){
+		Application.Quit ();
+	}
+
+
+
+}
